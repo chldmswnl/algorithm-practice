1dbf27d615fca611f0657b36597250ec19dd72bc
chldmswnl <chldmswnl12@gmail.com>
2022-08-24 14:04:01 +0900

    124나라의 숫자

---
+++ programmers_0824_02.js
@@ -0,0 +1,15 @@
+/**
+ * 프로그래머스 level 1. 124나라의 숫자
+ */
+
+
+function solution(n) {
+    var answer = '';
+    const mod=[4,1,2];
+    
+    while(n>0){
+        answer=mod[n%3]+answer; 
+        n=parseInt((n-1)/3);          
+    }
+    return answer;
+}
