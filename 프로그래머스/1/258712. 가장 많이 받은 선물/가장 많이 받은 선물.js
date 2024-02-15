5d2cd92229a0537e1737278d58b958cb8a84f3fe
최은지 (Eunji Choi) <63483751+chldmswnl@users.noreply.github.com>
2024-02-15 12:10:46 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 3.77 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,36 +1,19 @@
 function solution(friends, gifts) {
-    const len=friends.length;
-    const giftIndexArr=new Array(len).fill(0);
-    const nextMonth=new Array(len).fill(0);
-    const nameMap = new Map();
-    const giftTable = new Array(len).fill(0).map(_ => new Array(len).fill(0));
-
-    friends.forEach((name, idx) => {
-        nameMap.set(name, idx);
+    const giftIndexArr=new Array(friends.length).fill(0);
+    const exchangeHistory=new Array(friends.length).fill(new Array(friends.length).fill(0));
+    gifts.map(gift=>{
+        const arr=gift.split(" ");
+        const sender=arr[0];
+        const receiver=arr[1];
+        const senderIndex=friends.indexOf(sender);
+        const receiverIndex=friends.indexOf(receiver);
+        console.log(senderIndex, receiverIndex, exchangeHistory)
+        exchangeHistory[senderIndex][receiverIndex]++;
+        
+        console.log(exchangeHistory[0])
+        giftIndexArr[senderIndex]++;
+        giftIndexArr[receiverIndex]--;
     })
-    
-    gifts.forEach(info => {
-        const [from, to] = info.split(" ");
-
-        giftTable[nameMap.get(from)][nameMap.get(to)]++;
-
-        giftIndexArr[nameMap.get(from)]++;
-        giftIndexArr[nameMap.get(to)]--;
-    })
-
-    for(let i=0;i<giftTable.length;i++){
-        for(let j=i+1;j<giftTable[i].length;j++){
-            if(i===j) continue;
-            if(giftTable[i][j]>giftTable[j][i]) nextMonth[i]++;
-            if(giftTable[i][j]<giftTable[j][i]) nextMonth[j]++;
-            if(giftTable[i][j]===giftTable[j][i]){
-                if(giftIndexArr[i]>giftIndexArr[j]) nextMonth[i]++;
-                if(giftIndexArr[j]>giftIndexArr[i]) nextMonth[j]++
-            }
-            
-        }
-    };
-    
-
-    return Math.max(...nextMonth);
+  
+    return [];
 }
