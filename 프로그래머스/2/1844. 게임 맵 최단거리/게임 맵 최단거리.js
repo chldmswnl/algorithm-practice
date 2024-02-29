2f0fb86e197a1525f6009c15d40a8612b74d86d5
최은지 (Eunji Choi) <63483751+chldmswnl@users.noreply.github.com>
2024-02-29 12:08:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.72 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,20 @@
+function solution(maps) {
+    const n=maps[0].length-1;
+    const m=maps.length-1;
+    const queue=[[0,0,1]];
+    
+    while(queue.length){
+        const [x,y,move]=queue.shift();
+        if(x===n && y===m) return move;
+        if(!maps[y][x]) continue;
+        maps[y][x]=false;
+        
+        if(x-1>=0 && maps[y][x-1] ) queue.push([x-1,y,move+1]);
+        if(x+1<=n && maps[y][x+1] ) queue.push([x+1,y,move+1]);
+        if(y-1>=0 && maps[y-1][x] ) queue.push([x,y-1,move+1]);
+        if(y+1<=m && maps[y+1][x] ) queue.push([x,y+1,move+1]);
+        
+    }
+
+    return -1;
+}
